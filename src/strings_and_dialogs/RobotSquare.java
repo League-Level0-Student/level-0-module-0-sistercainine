2917aa166d0469519229c10fa79592577e681a95
League Student <league@iMac-4.attlocal.net>
2018-09-19 18:22:28 -0700

    robot square

--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -12,22 +12,27 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
-
+Robot shrek=new Robot ();
         // 3. Put the robot's pen down
-
+shrek.penDown();
 
         // 6. Make the robot move as fast as possible
+shrek.setSpeed(1000);
 
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4 ; i++) {
+	
 
+shrek.move(200);
+shrek.turn(360/4);
+}
 
         //         2. Move your robot 200 pixels
-
+shrek.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+shrek.turn(90);
 
     }
 }
