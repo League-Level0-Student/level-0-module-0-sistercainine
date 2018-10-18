58c7d987fd86be0e654846343e43de43058594bf
League Student <league@iMac-4.attlocal.net>
2018-10-17 18:24:31 -0700

    Robot Graffiti
    1 Robot Graffiti
    Oct 17, 2018

--- src/strings_and_dialogs/MyJavaProgram.java
+++ src/strings_and_dialogs/MyJavaProgram.java
@@ -7,8 +7,26 @@ public class MyJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+		Robot shrek=new Robot ();
+		shrek.sparkle();
+				shrek.setSpeed(1000);
+		shrek.penDown();
+		shrek.move(200);
+		shrek.turn(90);
+		shrek.penDown();
+        shrek.move(130);
+		shrek.penDown();
+		shrek.penDown();
+        shrek.move(-130);
+	    shrek.turn(90);  
+	    shrek.move(100);
+	    shrek.turn(-90);
+	    shrek.move(110);
+	    
+	    
+	    
 	}
+	
+	
+	
 }
